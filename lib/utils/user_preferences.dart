7f8a354f58765593d00edd30e18071f88bcc788a
fauzanr644 <fauzanr644@gmail.com>
2021-05-02 15:19:19 +0700

    mp2

---
+++ lib/utils/user_preferences.dart
@@ -0,0 +1,13 @@
+import 'package:user_profile_ii_example/model/user.dart';
+
+class UserPreferences {
+  static const myUser = User(
+    imagePath:
+        'https://images.unsplash.com/photo-1585007600338-ec568e187cc1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    name: 'Fauzan Rizalul Ulum',
+    email: 'fauzanr644@gmail.com',
+    about:
+        'Et sint duis elit ullamco quis adipisicing ut nisi laboris sint ipsum aute est. Aliqua labore adipisicing ullamco aliquip incididunt cillum exercitation culpa exercitation mollit Lorem.',
+    isDarkMode: false,
+  );
+}
